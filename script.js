32051468b8df2e93e152ed0d73e399508ff35297
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-02-15 20:00:33 -0600

    Create check for player victory.

--- script.js
+++ script.js
@@ -5,12 +5,56 @@ function tictactoe() {
     cell.addEventListener("click", playerMove, { once: true });
   });
 
+  let playerCells = [];
+  let opponentCells = [];
+
+  function addCells(id, array) {
+    const cellIndex = Array.from(cells).indexOf(document.getElementById(id));
+    array.push(cellIndex);
+  }
+
+  const banner = document.getElementById("winner");
+
+  const winningCombinations = [
+    [0, 1, 2], // horizontal
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // vertical
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // diagonal
+    [2, 4, 6],
+  ];
+
+  function checkWinner(selectedCells) {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (
+        selectedCells.includes(a) &&
+        selectedCells.includes(b) &&
+        selectedCells.includes(c)
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   function playerMove(e) {
     const cell = e.target;
     if (cell.textContent == "X" || cell.textContent == "O") {
       return;
     }
+
     cell.textContent = "X";
+    addCells(cell.id, playerCells);
+    if (checkWinner(playerCells)) {
+      banner.textContent = "PLAYER WINS";
+      cells.forEach((cell) => {
+        cell.removeEventListener("click", playerMove);
+      });
+      return;
+    }
     setTimeout(function () {
       opponentMove();
     }, 300);
@@ -31,6 +75,7 @@ function tictactoe() {
         continue;
       } else {
         cellArray[randomCell].textContent = "O";
+        addCells(randomCell, opponentCells);
         break;
       }
     }
